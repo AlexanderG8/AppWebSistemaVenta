bd94f4439b3ba9b84d63e260fe41828e1ace5735
Alexander Gomez <mpalexanderg@gmail.com>
2022-02-17 00:57:12 -0500

    MGG: Mantenimiento Clientes

--- WebApplication1/WebApplication1/Controllers/ClienteController.cs
+++ WebApplication1/WebApplication1/Controllers/ClienteController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication1.Models;
+using WebApplication1.Models.Request;
+using WebApplication1.Models.Response;
 
 namespace WebApplication1.Controllers
 {
@@ -12,14 +14,93 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class ClienteController : ControllerBase
     {
-        [HttpGet]
+        [HttpGet("Lista")]
         public IActionResult Get() 
         {
-            using (BDVentasContext db = new BDVentasContext()) 
+            Respuesta oRespuesta = new Respuesta();
+            try
             {
-                var lista = db.Clientes.ToList();
-                return Ok(lista);
+                using (BDVentasContext db = new BDVentasContext())
+                {
+                    var lista = db.Clientes.ToList();
+                    oRespuesta.Exito = 1;
+                    oRespuesta.Data = lista;
+                }
             }
+            catch (Exception ex)
+            {
+
+                oRespuesta.Mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
+        }
+
+        [HttpPost("Guardar")]
+        public IActionResult Add(ClienteRequest oModel)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                using (BDVentasContext db = new BDVentasContext())
+                {
+                    Cliente oCliente = new Cliente();
+                    oCliente.Nombre = oModel.Nombre;
+                    db.Clientes.Add(oCliente);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpPut("Editar")]
+        public IActionResult Edit(ClienteRequest oModel)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                using (BDVentasContext db = new BDVentasContext())
+                {
+                    Cliente oCliente = db.Clientes.Find(oModel.id);
+                    oCliente.Nombre = oModel.Nombre;
+                    db.Clientes.Update(oCliente);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpDelete("Eliminar/{id}")]
+        public IActionResult Delete(int id)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                using (BDVentasContext db = new BDVentasContext())
+                {
+                    Cliente oCliente = db.Clientes.Find(id);
+                    db.Remove(oCliente);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+            return Ok(oRespuesta);
         }
     }
 }
---
+++ WebApplication1/WebApplication1/Models/Request/ClienteRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models.Response;
+
+namespace WebApplication1.Models.Request
+{
+    public class ClienteRequest
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
